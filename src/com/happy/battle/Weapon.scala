5f65e19eee8c3ab23654f6f66d19f7e7e080edd5
qinzhaoyang <qinzhaoyang@inspur.com>
2019-09-09 13:26:12 +0800

    fix comments

--- src/com/happy/battle/Weapon.scala
+++ src/com/happy/battle/Weapon.scala
@@ -61,7 +61,7 @@ object BigKnife extends Weapon {
 
   override def effect(hero: Hero, hero2: Hero): Unit = {
     this.damage = hero2.current() / 2
-    println(s"~~~一刀半管血，就问你怕不怕？##${this.damage}##")
+    println(s"~一刀半管血，问你怕不怕？##${this.damage}##")
   }
 }
 
@@ -86,8 +86,8 @@ object Boom extends Weapon {
   override def effect(hero: Hero, hero2: Hero): Unit = {
     val bingo = Random.nextDouble()
     if (bingo >= 0.3 && bingo <= 0.7) {
-      println("\"没有什么一发解决不了的问题!\"\n如果有，那就再来一发！（命中要害，++Double）")
       this.damage = this.damage * 2
+      println("\"没有什么一发解决不了的问题!\"\n\"如果有，那就再来一发！\"（命中要害，伤害翻倍！）")
     }
   }
 
@@ -98,7 +98,7 @@ object Java extends Weapon {
   this.damage = -50
 
   override def toString: String = {
-    "扔出一本《【JAVA】编程思想》！对方阅读后血量增加了[50]！o(╯□╰)o"
+    "扔出一本《【JAVA】编程思想》！\n不料对方[空手接白书]，阅读后血量增加了[50]！o(╯□╰)o"
   }
 }
 
@@ -108,12 +108,12 @@ object Scala extends Weapon {
   val cure = 90
 
   override def toString: String = {
-    "学完《快学【Scala】》，修改了自己的血量+100！O(∩_∩)O哈哈~\n 对方精神损失：" + this.damage
+    s"学完《快学【Scala】》，修改了自己的血量+${this.cure}！\nO(∩_∩)O哈哈~ 对方精神损失：" + this.damage
   }
 
   override def effect(hero: Hero, hero2: Hero): Unit = {
     hero.health = hero.health + this.cure
-    println(s"嘿嘿(*^▽^*)，我发现修改血量的代码在第7行~~悄悄把自己血量改成了【${hero.current()}】")
+    println(s"嘿嘿(*^▽^*)，发现修改血量的代码在第7行~~我悄悄把自己血量改成了【${hero.current()}】")
   }
 }
 
@@ -145,7 +145,7 @@ object Python extends Weapon {
   val selfDamage: Int = this.damage / 2
 
   override def toString: String = {
-    "[杀敌一千，自损八百！]" + this.damage
+    "人生苦短，我用【Python】~" + this.damage
   }
 
   override def effect(hero: Hero, hero2: Hero): Unit = {
@@ -153,6 +153,6 @@ object Python extends Weapon {
     if (hero.health <= 0) {
       hero.health = 1
     }
-    println(s"人生苦短，我用【Python】~~ \n自己还剩【${hero.current()}】血量！")
+    println(s"[杀敌一千，自损八百！]~ \n自己还剩【${hero.current()}】血量！")
   }
 }
